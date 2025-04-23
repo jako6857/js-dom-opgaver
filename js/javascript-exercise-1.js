3da47f1b1d20d4a2ba56f75d0d1399c39599921e
jako6857 <82064889+jako6857@users.noreply.github.com>
2025-04-23 08:14:39 +0200

    første 3

--- js/javascript-exercise-1.js
+++ js/javascript-exercise-1.js
@@ -3,19 +3,19 @@
  * Tilføjer/fjerner en css klasse med større skrifttype til alle elementer
  * Benytter metoden addEventListener til at lytte på et onclick event på knappen
  */
-document.getElementById("example1").addEventListener("click", function() {
-    //Henter array med alle child elementer til #contentarea
-    let elements = document.querySelectorAll("#contentarea *");
-    //Lopper array med elementer
-    for(let i = 0; i < elements.length; i++) {
-        //Tilføj klassen tx-big hvis den ikke eksisterer i et elements classList
-        if(!elements[i].classList.contains("tx-big")) {
-            elements[i].classList.add("tx-big");
-        } else {
-            //Ellers fjern klassen hvis den eksisterer i elementets classList
-            elements[i].classList.remove("tx-big");
-        }
+document.getElementById("example1").addEventListener("click", function () {
+  //Henter array med alle child elementer til #contentarea
+  let elements = document.querySelectorAll("#contentarea *");
+  //Lopper array med elementer
+  for (let i = 0; i < elements.length; i++) {
+    //Tilføj klassen tx-big hvis den ikke eksisterer i et elements classList
+    if (!elements[i].classList.contains("tx-big")) {
+      elements[i].classList.add("tx-big");
+    } else {
+      //Ellers fjern klassen hvis den eksisterer i elementets classList
+      elements[i].classList.remove("tx-big");
     }
+  }
 });
 
 /**
@@ -24,11 +24,33 @@ document.getElementById("example1").addEventListener("click", function() {
  * Anvender onclick metode direkte på getElementById metoden
  * Anvender forEach metode til at loope array med
  */
-document.getElementById("example2").onclick = function() {
-    //Henter array med alle child elementer til #contentarea og kører en foreach på arrayet
-    document.querySelectorAll("#contentarea *").forEach( function(element) {
-        //Toggler class tx-big til elementernes classList
-        element.classList.toggle("tx-big");
-    });
+document.getElementById("example2").onclick = function () {
+  //Henter array med alle child elementer til #contentarea og kører en foreach på arrayet
+  document.querySelectorAll("#contentarea *").forEach(function (element) {
+    //Toggler class tx-big til elementernes classList
+    element.classList.toggle("tx-big");
+  });
 };
 
+function skiftTekst() {
+  const heading = document.getElementById("myHeading");
+  if (heading.innerText === "Javascript - Opgaver 1") {
+    heading.innerText = "Jeg Ændrede Mig Altså Lige";
+  } else {
+    heading.innerText = "Javascript - Opgaver 1";
+  }
+}
+
+function skiftParagraf() {
+  const alleParagraffer = document.querySelectorAll("p");
+  alleParagraffer.forEach(function (paragraf) {
+    paragraf.style.color = "red";
+  });
+}
+
+function roterLi() {
+  const roterList = document.querySelectorAll("li");
+  roterList.forEach(function (li) {
+    li.classList.add("transform");
+  });
+}
